cb04315ea64cf5f9dd41d6aed32d1070cbada7f1
himaf <marc_affechi@hotmail.com>
2021-08-08 23:09:56 +0000

    update for select data to predict

--- main.py
+++ main.py
@@ -28,6 +28,24 @@ def prediction(features):
     return ypred
 
 
+def get_data(query):
+    try:
+        sqliteConnection = sqlite3.connect('DATABASE.sqlite3')
+        cursor = sqliteConnection.cursor()
+        print("Database created and Successfully Connected to SQLite")
+
+        cursor.execute(query)
+        data = cursor.fetchall()
+        cursor.close()
+        return data
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+
 @app.get("/", response_class=HTMLResponse)
 async def get_index(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
@@ -37,9 +55,14 @@ async def get_index(request: Request):
 async def get_unique(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.get("/db_search", response_class=HTMLResponse)
 async def get_in_db(request: Request):
-    return templates.TemplateResponse("searchDb.html", {"request": request})
+    query = "SELECT CLIENTNUM, Customer_Age, Total_Relationship_Count, Total_Revolving_Bal, " \
+            "Total_Amt_Chng_Q4_Q1, Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1, " \
+            "Avg_Utilization_Ratio FROM CUSTOMERS ORDER BY random() LIMIT 200;"
+    data = get_data(query)
+    return templates.TemplateResponse("searchDb.html", {"request": request, "datas": data})
 
 
 @app.post("/predict")
@@ -56,25 +79,26 @@ async def predict(Customer_Age: int, Total_Relationship_Count: int, Total_Revolv
 
 @app.get("/predict")
 async def predict_group(search_count: int):
-    try:
-        sqliteConnection = sqlite3.connect('DATABASE.sqlite3')
-        cursor = sqliteConnection.cursor()
-        print("Database created and Successfully Connected to SQLite")
+    _query = f"SELECT Customer_Age, Total_Relationship_Count, Total_Revolving_Bal, " \
+             f"Total_Amt_Chng_Q4_Q1, Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1, " \
+             f"Avg_Utilization_Ratio, Attrition_Flag FROM CUSTOMERS ORDER BY random() LIMIT {search_count}; "
+    data = get_data(_query)
+    x_data = np.array(data)[:, :-1].tolist()
+    ypred = prediction(x_data)
 
-        _Query = f"SELECT Customer_Age, Total_Relationship_Count, Total_Revolving_Bal, " \
-                 f"Total_Amt_Chng_Q4_Q1, Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1, " \
-                 f"Avg_Utilization_Ratio, Attrition_Flag FROM CUSTOMERS ORDER BY random() LIMIT {search_count}; "
-        cursor.execute(_Query)
-        data = cursor.fetchall()
-        cursor.close()
+    return {'datas': data, 'pred': ypred}
+
+@app.get("/select_predict")
+async def selectPredict(searchID: str):
+    searchID = np.array(searchID).tolist()
+    print(searchID)
+    _query = f"SELECT Customer_Age, Total_Relationship_Count, Total_Revolving_Bal, " \
+             f"Total_Amt_Chng_Q4_Q1, Total_Trans_Amt, Total_Trans_Ct, Total_Ct_Chng_Q4_Q1, " \
+             f"Avg_Utilization_Ratio, Attrition_Flag FROM CUSTOMERS WHERE CLIENTNUM in {searchID};"
+
+    data = get_data(_query)
+    if data:
         x_data = np.array(data)[:, :-1].tolist()
         ypred = prediction(x_data)
-
         return {'datas': data, 'pred': ypred}
-
-    except sqlite3.Error as error:
-        print("Error while connecting to sqlite", error)
-    finally:
-        if sqliteConnection:
-            sqliteConnection.close()
-            print("The SQLite connection is closed")
+    return {'datas': [], 'pred': []}
