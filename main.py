c5dd45c00e3c97234b35a87052215392ee2e3f6e
himaf <marc_affechi@hotmail.com>
2021-08-17 10:45:20 +0000

    mise ajouter pour la version 3.0 ajouter d'import de fichier CSV xls xlsx txt

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import pickle
-
+import os
 import sqlite3
-
+from io import StringIO
 import pandas as pd
 import numpy as np
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, UploadFile, File, Query
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    columns: list = Query([])
+    rows: list = Query([])
+
 
 app = FastAPI()
 app.mount("/templates/assets", StaticFiles(directory="templates/assets"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+col = ['Customer_Age', 'Total_Relationship_Count',
+       'Total_Revolving_Bal', 'Total_Amt_Chng_Q4_Q1',
+       'Total_Trans_Amt', 'Total_Trans_Ct',
+       'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio']
+
 
 def prediction(features):
-    col = ['Customer_Age', 'Total_Relationship_Count',
-           'Total_Revolving_Bal', 'Total_Amt_Chng_Q4_Q1',
-           'Total_Trans_Amt', 'Total_Trans_Ct',
-           'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio']
     # creer le DataFrame
     data = pd.DataFrame(features, columns=col)
     model = pickle.load(open('creditCard_modelRFC.sav', 'rb'))
@@ -29,6 +38,7 @@ def prediction(features):
 
 
 def get_data(query):
+    sqliteConnection = None
     try:
         sqliteConnection = sqlite3.connect('DATABASE.sqlite3')
         cursor = sqliteConnection.cursor()
@@ -65,6 +75,11 @@ async def get_in_db(request: Request):
     return templates.TemplateResponse("searchDb.html", {"request": request, "datas": data})
 
 
+# @app.get("/import", response_class=HTMLResponse)
+# async def get_in_file(request: Request):
+#     return templates.TemplateResponse("import.html", {"request": request})
+
+
 @app.post("/predict")
 async def predict(Customer_Age: int, Total_Relationship_Count: int, Total_Revolving_Bal: int,
                   Total_Amt_Chng_Q4_Q1: float, Total_Trans_Amt: int, Total_Trans_Ct: int, Total_Ct_Chng_Q4_Q1: float,
@@ -88,6 +103,7 @@ async def predict_group(search_count: int):
 
     return {'datas': data, 'pred': ypred}
 
+
 @app.get("/select_predict")
 async def selectPredict(searchID: str):
     searchID = np.array(searchID).tolist()
@@ -102,3 +118,65 @@ async def selectPredict(searchID: str):
         ypred = prediction(x_data)
         return {'datas': data, 'pred': ypred}
     return {'datas': [], 'pred': []}
+
+
+# @app.get("/file-upload", response_class=HTMLResponse)
+# async def readFile(request: Request, file: UploadFile = File(...)):
+#     ext = os.path.splitext(file.filename)[1]
+#     data = []
+#     if ext == '.csv':
+#         df = pd.read_csv(StringIO(file.file.read().decode('utf-8')), sep=";")
+#         data.append(df.columns.tolist())
+#         data.append(df.values.tolist())
+#         # print(df.head())
+#     elif ext == '.xls' or ext == '.xlsx':
+#         df = pd.read_excel(StringIO(file.file.read().decode('utf-8')), encoding='utf-8')
+#     elif ext == '.txt':
+#         df = pd.read_table(StringIO(file.file.read().decode('utf-8')), encoding='utf-8')
+#     else:
+#         return {'error': 'file extension is not supported'}
+#     return templates.TemplateResponse("import.html", {"request": request, "datas": data})
+
+
+@app.post("/file-upload")
+async def readFile(file: UploadFile = File(...)):
+    ext = os.path.splitext(file.filename)[1]
+    data = []
+
+    if ext == '.csv':
+        df = pd.read_csv(StringIO(file.file.read().decode('utf-8')), sep=";")
+        err = [c for c in col if c not in df.columns.tolist()]
+        if len(err):
+            return {'error': {'msg': "le fichier de données n'est pas compatible", 'details': err}}
+
+    elif ext in ['.xls', '.xlsx']:
+        df = pd.read_excel(StringIO(file.file.read().decode('utf-8')), encoding='utf-8')
+        err = [c for c in col if c not in df.columns.tolist()]
+        if len(err):
+            return {'error': {'msg': "le fichier de données n'est pas compatible", 'details': err}}
+
+    elif ext == '.txt':
+        df = pd.read_table(StringIO(file.file.read().decode('utf-8')), encoding='utf-8')
+        err = [c for c in col if c not in df.columns.tolist()]
+        if len(err):
+            return {'error': {'msg': "le fichier de données n'est pas compatible", 'details': err}}
+
+    else:
+        return {'error': {'msg': 'file extension is not supported', 'details': []}}
+
+    data.append(df.columns.tolist())
+    data.append(df.values.tolist())
+
+    return {"datas": data}
+
+
+@app.post("/file_predict")
+async def filePredict(item: Item):
+
+    item.columns.insert(0, "checkbox")
+    df = pd.DataFrame(item.rows, columns=item.columns)
+
+    ypred = prediction(df[col])
+
+    return {'col': df[col].columns.tolist(), 'datas': df[col].values.tolist(), 'pred': ypred}
+
--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -16,6 +16,321 @@
     } catch (err) {
         console.log(err);
     }
-    
 
 })(jQuery);
+
+Dropzone.options.dataFile = {
+    acceptedFiles: ".csv, .xls, .xlsx"
+}
+
+// var dropzone = new Dropzone('#dataFile', {
+//     previewTemplate: document.querySelector('#preview-template').innerHTML,
+//     url: "/file-upload/",
+//     thumbnailHeight: 120,
+//     thumbnailWidth: 120,
+//     maxFilesize: 3,
+//     maxFiles: 1,
+//     acceptedFiles: '.csv,.xls, .xlsx',
+//     addRemoveLinks: true,
+//     filesizeBase: 1000,
+//     thumbnail: function(file, dataUrl) {
+//         if (file.previewElement) {
+//             file.previewElement.classList.remove("dz-file-preview");
+//             var images = file.previewElement.querySelectorAll("[data-dz-thumbnail]");
+//             for (var i = 0; i < images.length; i++) {
+//                 var thumbnailElement = images[i];
+//                 thumbnailElement.alt = file.name;
+//                 thumbnailElement.src = dataUrl;
+//             }
+//             setTimeout(function() { file.previewElement.classList.add("dz-image-preview"); }, 1);
+//         }
+//     }
+//
+// });
+//
+//
+// // Now fake the file upload, since GitHub does not handle file uploads
+// // and returns a 404
+//
+// var minSteps = 6,
+//     maxSteps = 60,
+//     timeBetweenSteps = 100,
+//     bytesPerStep = 100000;
+//
+// dropzone.uploadFiles = function(files) {
+//     var self = this;
+//
+//     for (var i = 0; i < files.length; i++) {
+//
+//         var file = files[i];
+//         totalSteps = Math.round(Math.min(maxSteps, Math.max(minSteps, file.size / bytesPerStep)));
+//
+//         for (var step = 0; step < totalSteps; step++) {
+//             var duration = timeBetweenSteps * (step + 1);
+//             setTimeout(function(file, totalSteps, step) {
+//                 return function() {
+//                     file.upload = {
+//                         progress: 100 * (step + 1) / totalSteps,
+//                         total: file.size,
+//                         bytesSent: (step + 1) * file.size / totalSteps
+//                     };
+//
+//                     self.emit('uploadprogress', file, file.upload.progress, file.upload.bytesSent);
+//                     if (file.upload.progress == 100) {
+//                         file.status = Dropzone.SUCCESS;
+//                         self.emit("success", file, 'success', null);
+//                         self.emit("complete", file);
+//                         self.processQueue();
+//                     }
+//                 };
+//             }(file, totalSteps, step), duration);
+//         }
+//     }
+// };
+//
+// dropzone.on("addedfile", function(file) {
+//    console.log(file.fullPath);
+// });
+
+var dataFile = new Dropzone("#dataFile");
+
+dataFile.on("sending", function(file) {
+    //let ext = file.name.split('.')[1];
+
+    // if (ext == 'csv') {
+    var modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('import'));
+    modal.hide();
+
+    var modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('viewImport'));
+    $('#viewImport .modal-header').show();
+    modal.show();
+
+    // }
+});
+
+dataFile.on("success", function(file, res) {
+    <!-- Table head -->
+    var table;
+    table = '<thead class="sticky-top bg-white"><tr><th scope="col" class="check-all"><input type="checkbox" class="custom-control-input" id="checkAll" style="width: 12px"></th>';
+
+    res.datas[0].forEach(function(data) {
+        table += '<th scope="col">'+data+'</th>';
+    });
+    table += '</tr></thead><tbody>';
+    res.datas[1].forEach(function (data, i) {
+        table += '<tr> \
+                        <th scope="row"> \
+                        </th>';
+        data.forEach(function (el) {
+            table += '<td>'+el+'</td>';
+        });
+        table += '</tr>';
+    });
+    table += '</tbody>';
+
+    $('#dbTable1').html(table);
+
+    var tableLoad
+    tableLoad = $('#dbTable1').DataTable({
+        searching: false,
+        lengthChange: false,
+        rowId: 'ID client',
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style: 'multi',
+            selector: 'th:first-child'
+        },
+        order: [
+            [1, 'desc']
+        ],
+        language: {
+            url: 'templates/assets/vendor/datatable/French.json'
+        }
+    });
+
+    tableLoad.on("click", "th.select-checkbox.check-all", function() {
+        if ($("th.select-checkbox.check-all").hasClass("selected")) {
+            tableLoad.rows().deselect();
+            $("th.select-checkbox.check-all").removeClass("selected");
+            $("#checkAll").prop("checked", false);
+        } else {
+            tableLoad.rows().select();
+            $("th.select-checkbox.check-all").addClass("selected");
+            $("#checkAll").prop("checked", true);
+        }
+    }).on("select deselect", function() {
+        ("Some selection or deselection going on")
+
+        if (tableLoad.rows({
+            selected: true
+        }).count() == 0){
+            $("#checkAll").prop("checked", false);
+            $("#checkAll").prop("indeterminate", false);
+            $("#search").hide();
+        }
+        else if (tableLoad.rows({
+            selected: true
+        }).count() !== tableLoad.rows().count()) {
+            // $("th.select-checkbox.check-all").removeClass("selected");
+            $("#checkAll").prop("checked", false);
+            $("#checkAll").prop("indeterminate", true);
+            $("#search").html('<i class="fa fa-search" aria-hidden="true" ></i> Recherchez '+tableLoad.rows({selected: true}).count()+' Prédictions');
+            $("#search").show();
+
+        }
+        else {
+            // $("th.select-checkbox.check-all").addClass("selected");
+            $("#checkAll").prop("checked", true);
+            $("#search").html('<i class="fa fa-search" aria-hidden="true" ></i> Recherchez toutes les Prédictions');
+            $("#search").show();
+
+        }
+    });
+
+
+
+
+    $('#predForm1').on('submit', function (e) {
+        e.preventDefault();
+        $('#viewImport #spinner').show();
+        $('#dbTable1').hide();
+        var rows = tableLoad.rows({selected: true }).data().toArray();
+
+        $('#spinner').css('display', 'flex');
+        $('.table-all').hide();
+        // $('#no-data').hide();
+        var el = $('#predForm1');
+        // var data = el.serialize();
+        el.hide();
+        var tableData = '';
+
+        $.ajax({
+            type: 'POST',
+            url: '/file_predict',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({columns: res.datas[0], rows: rows}),
+            success: function (res) {
+                tableData = '<thead class="sticky-top bg-white"><tr><th scope="col" ></th>';
+
+                res.col.forEach(function(data) {
+                    tableData += '<th scope="col">'+data+'</th>';
+                });
+                tableData += '<th scope="col">Prédiction</th>';
+                tableData += '</tr></thead><tbody>';
+                res.datas.forEach(function(data, i) {
+                    tableData += '<tr><th scope="row">'+(i+1)+'</th>';
+
+                    data.forEach(function(d, index) {
+                        if (index == 7) {
+                            var y = res.pred[i];
+                            if (y == 1) {
+                                y = "Existing Customer";
+                                var ycolor = 'background-color: #d63384;';
+                            }
+                            else {
+                                y = "Attrited Customer";
+                                var ycolor = 'background-color: #20c997;';
+                            };
+
+                            tableData+='<td>'+d+'</td>';
+                            tableData+='<td><span class="badge rounded-pill" style="'+ycolor+'color: white;">'+y+'</span></td>';
+                        }
+                        else {
+                            tableData+='<td>'+d+'</td>';
+                        }
+
+                    });
+                    tableData += '</tr>';
+                });
+
+                $('#viewImport #dbTable1').DataTable().clear().destroy();
+                $('#viewImport .table-responsive.table-all #dbTable1').html('');
+                $('#viewImport .table-responsive #dbTable1').html(tableData);
+                $('#viewImport .table-responsive #dbTable1').DataTable({
+                    searching: false,
+                    lengthChange: false,
+                    language: {
+                        url: 'templates/assets/vendor/datatable/French.json'
+                    }
+                });
+                $('#dbTable1').show();
+                $('#spinner').hide();
+                $('#search').hide();
+                // var modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('viewImport'));
+                // modal.hide();
+                $('.table-all').show();
+                el.show();
+
+                // modal.show();
+                // $('#viewImport #dbTable2').DataTable();
+
+            }
+        });
+
+
+        // formData = new FormData()
+        // formData.append('columns', 'test')
+        // formData = "{'columns': 'test'}"
+        // fetch('/file_predict', {method: 'POST', body: formData})
+        //     .then(response => response.json()).then(data => {
+        //     console.log(data)
+        // }).catch(error => {
+        //     console.log(error)
+        // });
+
+        // $.ajax({
+        //     type: 'GET',
+        //     url: '/predict?'+data,
+        //     success: function (res) {
+        //         // console.log(res);
+        //         res.datas.forEach(function(data, i) {
+        //             tableData += '<tr><th scope="row">'+(i+1)+'</th>';
+        //
+        //             data.forEach(function(d, index) {
+        //                 if (index == 8) {
+        //                     var y = res.pred[i];
+        //                     if (y == 1) {
+        //                         y = "Existing Customer";
+        //                         var ycolor = 'background-color: #d63384;';
+        //                     }
+        //                     else {
+        //                         y = "Attrited Customer";
+        //                         var ycolor = 'background-color: #20c997;';
+        //                     };
+        //                     if (d == "Existing Customer") {
+        //                         var color = 'background-color: #d63384;';
+        //                     }  else {
+        //                         var color = 'background-color: #20c997;';
+        //                     };
+        //                     tableData+='<td><span class="badge rounded-pill" style="'+color+'color: white;">'+d+'</span></td>';
+        //                     tableData+='<td><span class="badge rounded-pill" style="'+ycolor+'color: white;">'+y+'</span></td>';
+        //                 }
+        //                 else {
+        //                     tableData+='<td>'+d+'</td>';
+        //                 }
+        //
+        //             });
+        //             tableData += '</tr>';
+        //         });
+        //         $('.table.table-striped tbody').html('');
+        //         $('.table.table-striped tbody').append(tableData);
+        //         $('#spinner').hide();
+        //         $('.table-responsive').show();
+        //         el.show();
+        //
+        //     }
+        // });
+    });
+
+    $('#viewImport #spinner').hide();
+    $('#dbTable1').show();
+
+});
+
+dataFile.on("complete", function(file) {
+    dataFile.removeFile(file);
+});
